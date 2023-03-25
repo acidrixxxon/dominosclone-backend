9fa0282a904c09281695858a390f8738271e49af
acidrixxxon <rizhenko.vadim.r1.fc@gmail.com>
2023-03-25 03:05:31 +0200

    backend changes

--- Routes/OrderRouter.js
+++ Routes/OrderRouter.js
@@ -2,20 +2,54 @@ import express from 'express'
 import OrderController from '../Controllers/OrderController.js'
 import authMiddleware from '../middlewares/auth-middleware.js'
 import Order from '../Models/OrderModel.js'
+import Stripe from 'stripe'
 
 const router = express.Router()
+const stripe = Stripe('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')
 
 router.post('/create',async (req,res) => {
   try {
     const { order: { cart,details }} = req.body.body
 
     const newOrder = await Order.create({cart,details})
-    
-    if (newOrder) return res.status(200).json({
+
+    if (details.customerData.paymentType.id === 12312) {
+      const session = await stripe.checkout.sessions.create({
+        mode: "payment",
+        payment_method_types: ["card"],
+        line_items: newOrder.cart.items.map(item => ({
+          price_data: {
+            currency: "uah",
+            product_data: {
+              name: item.fulltitle
+            },
+            unit_amount: item.price * 100
+          },
+          quantity: item.qty,
+        })),
+        mode: 'payment',
+        success_url: `${process.env.FRONTEND_URL}/order/${newOrder._id}`,
+        cancel_url: `${process.env.FRONTEND_URL}/order/${newOrder._id}`,
+      })
+      
+      const orderWithLink = {...newOrder._doc,details: {...newOrder._doc.details,customerData: {...newOrder._doc.details.customerData,paymentType: {...newOrder._doc.details.customerData.paymentType,paymentLink: session.url}}}}
+
+      const updatedOrder = await Order.findByIdAndUpdate(newOrder._id,{ ...orderWithLink},{new: true})
+
+      if (updatedOrder) return res.status(200).json({
+        message: 'Нове замовлення створене!',
+        success: true,
+        order: updatedOrder
+      })
+    } else {
+          if (newOrder) return res.status(200).json({
       message: 'Нове замовлення створене!',
       success: true,
       order: newOrder
     })
+    }
+
+
   } catch (error) {
     console.log(error.message)
 
--- index.js
+++ index.js
@@ -1,27 +1,62 @@
+import cors from 'cors'
+import dotenv from 'dotenv'
 import express from 'express'
 import { createServer } from 'http'
-import { Server } from 'socket.io'
-import cors from 'cors'
 import mongoose from 'mongoose'
-import PizzaRouter from './Routes/PizzaRouter.js'
-import SidesRouter from './Routes/SidesRouter.js'
-import DrinkRouter from './Routes/DrinkRouter.js'
-import OrderRouter from './Routes/OrderRouter.js'
-import UserRouter from './Routes/UserRouter.js'
-import SearchRouter from './Routes/SearchRouter.js'
-import dotenv from 'dotenv'
+import { Server } from 'socket.io'
+import Stripe from 'stripe'
+import ApiError from './middlewares/ApiError.js'
+import { errorMiddleware } from './middlewares/error-middleware.js'
+import Order from './Models/OrderModel.js'
 import SidesCategory from './Models/Sides/SidesCategoryModel.js'
 import Sides from './Models/Sides/SidesModel.js'
-import { errorMiddleware } from './middlewares/error-middleware.js'
+import DrinkRouter from './Routes/DrinkRouter.js'
+import OrderRouter from './Routes/OrderRouter.js'
+import PizzaRouter from './Routes/PizzaRouter.js'
+import SearchRouter from './Routes/SearchRouter.js'
+import SidesRouter from './Routes/SidesRouter.js'
+import UserRouter from './Routes/UserRouter.js'
 dotenv.config()
 
 const app = express()
+
+const stripe = Stripe(process.env.STRIPE_SECRET_KEY)
 
 
 app.use(express.json())
 app.use(cors())
 
+app.get('/payment/:id',async (req,res) => {
+  try {
+    const { id } = req.params
 
+    const order = await Order.findById(id)
+    console.log(order)
+
+    const session = await stripe.checkout.sessions.create({
+      mode: "payment",
+      payment_method_types: ["card"],
+      line_items: order.cart.items.map(item => ({
+        price_data: {
+          currency: "uah",
+          product_data: {
+            name: item.fulltitle
+          },
+          unit_amount: item.price * 100
+        },
+        quantity: item.qty,
+      })),
+      mode: 'payment',
+      success_url: `${process.env.FRONTEND_URL}/order/${id}`,
+      cancel_url: `${process.env.FRONTEND_URL}/order/${id}`,
+    })
+
+    res.json({url: session.url})
+  } catch (error) {
+    console.log(error)
+    res.status(500).json({error: error.message})
+  }
+})
 app.use('/sides',SidesRouter)
 app.use('/pizza',PizzaRouter)
 app.use('/drinks',DrinkRouter)
